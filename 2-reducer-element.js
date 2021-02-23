cd04f1193bb1edb0ad968cb283ea4263f06c3887
Abdul haris Alkarni <haris.alkarni@gmail.com>
2021-02-24 00:40:24 +0700

    Create 2-reducer-element.js

---
+++ 2-reducer-element.js
@@ -0,0 +1,16 @@
+var runningSum = function(nums) {
+    let a = []
+    for(let i = 0 ; i < nums.length ; i++) {
+        sum = 0;
+        for(let j = 0; j <= i; j++){
+            sum+= nums[j];
+        }
+        a.push(sum);
+    }
+    return a;
+};
+
+console.log(runningSum([1,2,3,4,5,6]));
+
+
+
