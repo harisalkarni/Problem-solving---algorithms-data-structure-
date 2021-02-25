24ca6909397e558886e89a7e5103f54603d2e810
Abdul haris Alkarni <haris.alkarni@gmail.com>
2021-02-25 20:46:13 +0700

    Create 3-max-boundaries-checker.js

---
+++ 3-max-boundaries-checker.js
@@ -0,0 +1,11 @@
+let candies = [2,3,5,1,3], extraCandies = 3;
+
+console.log( kidsWithCandies = function(candies, extraCandies) {
+    let a = [];
+    for(i = 0 ; i < candies.length; i++){
+         candies[i] + extraCandies >= Math.max(...candies) ? a.push (true) : a.push (false);
+    }
+    return a;
+    
+}(candies,extraCandies))
+
